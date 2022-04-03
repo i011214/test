4490c3a181bf405909c7f92e88d9a33f2208f089
i011214 <i011214@yahoo.co.jp>
2022-04-03 13:54:38 +0900

    すみっコぐらしが鳴るようにしました

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+basic.forever(function () {
+    music.playMelody("E G - C D E F F ", 120)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playMelody("E G - C D C C - ", 120)
+    music.rest(music.beat(BeatFraction.Whole))
+})
